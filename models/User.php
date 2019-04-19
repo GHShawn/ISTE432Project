db38af00ca4a78bb24981eaaa76f4eeb2dedec4d
LinJianChen <38051769+LinJianChen@users.noreply.github.com>
2019-04-19 13:39:08 -0400

    Update User.php

--- models/User.php
+++ models/User.php
@@ -1,19 +1,19 @@
 <?php
 
-class User {
+ class User {
     private $id;
     private $email;
     private $username;
 
-    public function __constructor() {
-
+    private function __constructor() {
     }
 
     public static function create($id, $email, $username) {
         $user = new User();
         $user->id = $id;
         $user->email = $email;
-        $uesr->username = $username;
+        $user->username = $username;
         return $user;
     }
 }
+?>
