be42fe25ee99a6f524aed878abf64aa91790d863
LinJianChen <38051769+LinJianChen@users.noreply.github.com>
2019-04-19 18:16:24 -0400

    combine files

--- main_website/assets/js/javascript.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-  	if(document.getElementById("myNav").style.display == "block"){
-		document.getElementById("myNav").style.display = "none";
-	}else{
-		document.getElementById("myNav").style.display = "block";
-	}
-}
