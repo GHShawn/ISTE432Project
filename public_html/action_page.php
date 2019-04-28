1086bac564019b22540b4395f2b4c46599154d62
sxw9470 <44449476+sxw9470@users.noreply.github.com>
2019-04-28 16:08:42 -0400

    Update action_page.php

--- public_html/action_page.php
+++ public_html/action_page.php
@@ -5,3 +5,5 @@ include_once(ROOT . 'business/IngredientManager.class.php');
 $manager = new IngredientManager();
 $igs = $manager->searchIngredients("apple");
 echo json_encode($igs);
+$comments= $_POST['comments'];
+?>
