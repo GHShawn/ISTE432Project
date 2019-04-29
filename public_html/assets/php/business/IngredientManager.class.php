073655e18c939021331e93ab4dd23b6cbf0c896a
Tenzin Dhondup <txd5857@g.rit.edu>
2019-04-29 00:06:24 -0400

    Backend 1 out of 3

--- public_html/assets/php/business/IngredientManager.class.php
+++ public_html/assets/php/business/IngredientManager.class.php
@@ -58,7 +58,7 @@ class IngredientManager {
         }
 
         $query2 = 'delete from shopping_list where user_id = 6';
-        $result = pg_query($this->conn, $query2);
+        $result = pg_execute($this->conn, $query2);
 
 
         pg_close($this->conn);
