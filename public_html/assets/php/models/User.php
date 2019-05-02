c0d8ad029f3f03cb7a5aea42a770ea2a87b8f5c3
LinJianChen <38051769+LinJianChen@users.noreply.github.com>
2019-05-01 21:14:13 -0400

    Update User.php

--- public_html/assets/php/models/User.php
+++ public_html/assets/php/models/User.php
@@ -1,19 +1,23 @@
 <?php
 
  class User {
-    public $id;
-    public $email;
-    public $username;
-
+    private  $id;
+    private  $email;
+    private  $username;
+    static $user = __CLASS__;
     private function __constructor() {
     }
 
     public static function create($id, $email, $username) {
-        $user = new User();
+        $user = new static();
         $user->id = $id;
         $user->email = $email;
         $user->username = $username;
         return $user;
+    }
+    public function getSome(){
+        $dum = get_object_vars($this);
+       return $dum;
     }
 }
 ?>
